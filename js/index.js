d64f09819ea9138812b42e7e6961332be6811e55
lebinh181998 <75607950+lebinh181998@users.noreply.github.com>
2024-02-01 12:32:47 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,5 @@ const barsButton = document.getElementById("bars");
 const listBars = document.getElementById("list-bars");
 
 barsButton.addEventListener("click", function () {
-  //   listBars.style.display = "block";
   listBars.classList.toggle("bars-active");
 });
